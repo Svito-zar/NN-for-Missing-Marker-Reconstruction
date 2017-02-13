Fixed summaries for tensorboard and data normalization

DAE/code/ae/FlatAE.py
@@ -35,6 +35,8 @@ class FlatAutoEncoder(object):
     self.__shape = shape  # [input_dim,hidden1_dim,...,hidden_n_dim,output_dim]
     self.__num_hidden_layers = len(self.__shape) - 2
 
+    self.__curr_batch_size = FLAGS.batch_size
+
     self.__variables = {}
     self.__sess = sess
 
@@ -170,24 +172,26 @@ class FlatAutoEncoder(object):
     """
     print('\nExtracting middle layer for a test sequence...')
     
-    sess = self.__sess
+    with self.__sess.graph.as_default():
+       
+      sess = self.__sess
       
-    # get input sequnce
-    currSequence = read_file(input_seq_file_name)
+      # get input sequnce
+      currSequence = read_file(input_seq_file_name)
 
-    # define tensors
-    input_ = tf.placeholder(dtype=tf.float32,
-                                  shape=(None, FLAGS.DoF),
-                                  name='ae_input_pl')
-    # Define the size of current input sequence
-    self.__curr_batch_size = sess.run(tf.shape(input_ )[0], feed_dict={input_ : currSequence})
+      # define tensors
+      input_ = tf.placeholder(dtype=tf.float32,
+                                    shape=(None, FLAGS.DoF),
+                                    name='ae_input_pl')
+      # Define the size of current input sequence
+      self.__curr_batch_size = sess.run(tf.shape(input_ )[0], feed_dict={input_ : currSequence})
 
-    # Define on an operator
-    middle_op = self.run_net(input_ , 1, just_middle = True) # 1 means that we have no dropout
-      
-    # for each snippet in a sequence
-    # pass through the network untill the middle layer
-    middle = sess.run(middle_op, feed_dict={input_: currSequence})
-      
-    # save it into a file
-    sio.savemat(output_seq_file_name, {'trialId':name, 'spikes':np.transpose(middle)})  
+      # Define on an operator
+      middle_op = self.run_net(input_ , 1, just_middle = True) # 1 means that we have no dropout
+        
+      # for each snippet in a sequence
+      # pass through the network untill the middle layer
+      middle = sess.run(middle_op, feed_dict={input_: currSequence})
+        
+      # save it into a file
+      sio.savemat(output_seq_file_name, {'trialId':name, 'spikes':np.transpose(middle)})  

DAE/code/ae/HierarchicalAE.py
@@ -323,15 +323,15 @@ class HierarchicalAE(object):
           second_hidden_layer.append(self._activate(hierarchical_hidden_layer[ind], self["w_"+name_of_part], self["b_"+name_of_part]))
 
         # Combine the outputs
-        upper_body_in = tf.concat(1, [second_hidden_layer[0], second_hidden_layer[1]])
-        lower_body_in = tf.concat(1, [second_hidden_layer[2], second_hidden_layer[3]])
+        upper_body_in = tf.concat(1, [second_hidden_layer[0], second_hidden_layer[1]],name = 'upper_body_in')
+        lower_body_in = tf.concat(1, [second_hidden_layer[2], second_hidden_layer[3]],name = 'lower_body_in')
 
         ################    Thirsd layer encoding    #################
         upper_body_out = self._activate(upper_body_in, self["w_upper_body"], self["b_upper_body"])
         lower_body_out = self._activate(lower_body_in, self["w_lower_body"], self["b_lower_body"])
 
         # Concatanate
-        whole_body_in = tf.concat(1, [body_in, upper_body_out, lower_body_out])
+        whole_body_in = tf.concat(1, [body_in, upper_body_out, lower_body_out],name = 'whole_body_in')
 
         ################    4th layer encoding    ##################
         representation = self._activate(whole_body_in, self["w_whole_body"], self["b_whole_body"])
@@ -352,9 +352,9 @@ class HierarchicalAE(object):
         indices = [ [ [elem, index] for index in range(self._body_channels+FLAGS.upper_body_neurons, 6+FLAGS.upper_body_neurons+FLAGS.lower_body_neurons,1) ] for elem in range(self.__curr_batch_size)]
         lower_body_decode = tf.gather_nd(whole_body_decode, indices, name=None) # changes the whole body"""
 
-        body_decode = tf.slice(whole_body_decode, [0,0], [self.__curr_batch_size,self._body_channels])
-        upper_body_slice = tf.slice(whole_body_decode, [0,self._body_channels], [self.__curr_batch_size,FLAGS.upper_body_neurons])
-        lower_body_slice = tf.slice(whole_body_decode, [0,self._body_channels+FLAGS.upper_body_neurons], [self.__curr_batch_size,FLAGS.lower_body_neurons])
+        body_decode = tf.slice(whole_body_decode, [0,0], [self.__curr_batch_size,self._body_channels], name = 'body_decode')
+        upper_body_slice = tf.slice(whole_body_decode, [0,self._body_channels], [self.__curr_batch_size,FLAGS.upper_body_neurons], name = 'upper_body_slice')
+        lower_body_slice = tf.slice(whole_body_decode, [0,self._body_channels+FLAGS.upper_body_neurons], [self.__curr_batch_size,FLAGS.lower_body_neurons], name = 'lower_body_slice')
 
         ################    2nd layer decoding    ##################
         upper_body_decode = self._activate(upper_body_slice, self["w_upper_body_decode"], self["b_upper_body_decode"])
@@ -379,7 +379,7 @@ class HierarchicalAE(object):
                               tf.slice(spine_and_l_leg_decode,[0,0],[self.__curr_batch_size, self.__encode1[0]]) )"""
 
         # Slice it back to the body parts
-        spine_slice = tf.slice(upper_body_decode,[0,0],[self.__curr_batch_size, self.__encode1[0]])
+        spine_slice = tf.slice(upper_body_decode,[0,0],[self.__curr_batch_size, self.__encode1[0]], name = 'spine_decode_slice')
         r_arm_slice = tf.slice(upper_body_decode,[0,self.__encode1[0]],[self.__curr_batch_size, self.__encode1[1]])
         l_arm_slice = tf.slice(upper_body_decode,[0,self.__encode1[0]+self.__encode1[1]],[self.__curr_batch_size, self.__encode1[2]])
         r_leg_slice = tf.slice(lower_body_decode,[0,0],[self.__curr_batch_size, self.__encode1[3]])

DAE/code/ae/utils/data.py
@@ -17,11 +17,6 @@ sys.path.append('/home/taras/Documents/Code/BVH/parser') # address of the BVH pa
 from reader import MyReader
 
 
-def _read32(bytestream):
-  dt = np.dtype(np.uint32).newbyteorder('>')
-  return np.frombuffer(bytestream.read(4), dtype=dt)
-
-
 def dense_to_one_hot(labels_dense, num_classes=10):
   """Convert class labels from scalars to one-hot vectors."""
   num_labels = labels_dense.shape[0]
@@ -193,15 +188,24 @@ def read_unlabeled_data(train_dir, amount_of_subfolders):
   new_size = amount_of_frames - (amount_of_frames%FLAGS.batch_size)
   input_data = input_data[:new_size]
 
+  
   # Do mean normalization : substract mean pose
   print('Normalizing the data ...')
   mean_pose = input_data.mean(axis=0)
   input_data = input_data - mean_pose [np.newaxis,:]
 
   # Scales all values in the input_data to be between -1 and 1
-  max_val = input_data.max()
-  eps=1e-8
-  input_data *= 1.0 / (max_val + eps)
+  max_val = np.amax(np.absolute(input_data), axis=0)
+  eps=1e-15
+  input_data =np.divide(input_data,max_val[np.newaxis,:]+eps)
+
+  # Chech the data range
+  max_ = input_data.max()
+  min_ = input_data.min()
+
+  #DEBUG
+  print("MAximum value in the normalized dataset : " + str(max_))
+  print("Minimum value in the normalized dataset : " + str(min_))
 
   # Get a standart deviation
   sigma = np.std(input_data, axis=0)
@@ -239,8 +243,6 @@ def read_unlabeled_data(train_dir, amount_of_subfolders):
 ''' Add Gaussian random vectors with zero mean and given variance '''
 def _add_noise(x, variance_multiplier, sigma):
   x_cp = np.copy(x)
-  #print(x.shape)
-  #print(np.multiply(sigma, variance_multiplier))
   eps=1e-15
   x_cp = x_cp + np.random.normal(0, np.multiply(sigma, variance_multiplier) + eps,(x_cp.shape[0],
                                   x_cp.shape[1]))